d49ae47f6f95ec0e85009529e3d5126bedbea211
Hson <hoangson1295@gmail.com>
2022-01-11 22:08:10 +0700

    vision2

--- BT4H_khoangCach/bt4h.js
+++ BT4H_khoangCach/bt4h.js
@@ -1,18 +1,15 @@
-function khoangCach() {
+function tinhKhoangCach() {
   // Get the value of the input field with id="numb"
-  var toaDoSV1x = document.getElementById("sinhVien1x").value * 1;
-  var toaDoSV1y = document.getElementById("sinhVien1y").value * 1;
   var toaDoTruongx = document.getElementById("truongx").value * 1;
   var toaDoTruongy = document.getElementById("truongy").value * 1;
-  var c = Math.pow((toaDoTruongx-toaDoSV1x), 2) + Math.pow((toaDoTruongy-toaDoSV1y), 2);
-  var b=Math.sqrt(c)
-  document.getElementById("xuatKetQua").innerText = "Số Chẵn Là : " + b;
-
-  //var result = (num1value + num2value + num3value + num4value + num5value) / 5;
-  //console.log({ y });
-  // document.write("tien luong la :" + c);
-  //document.getElementById("xuatSoChan").innerText =
-  // "Số Chẵn Là : " + soChan;
-  /*  document.getElementById("xuatTamGiac").innerText =
-     "Đây là tam giác : "; */
+  var toaDoSV1x = document.getElementById("sinhVien1x").value * 1;
+  var toaDoSV1y = document.getElementById("sinhVien1y").value * 1;
+  var hoTenSV1Value = document.getElementById("hoTenSV1").value;
+  var sinhvien1 =
+    Math.pow(toaDoTruongx - toaDoSV1x, 2) +
+    Math.pow(toaDoTruongy - toaDoSV1y, 2);
+  var tinhKhoangCachSV1 = Math.sqrt(sinhvien1);
+  document.getElementById("xuatKetQua").innerText =
+    "Số Chẵn Là : " + tinhKhoangCachSV1;
+  document.getElementById("xuatHoTen").innerText = hoTenSV1Value;
 }
